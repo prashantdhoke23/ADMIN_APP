23f553e9c4422182ad683de65b67fc549a080989
raja <raja.m@zensar.com>
2022-07-18 18:37:04 +0530

    init

---
+++ Admin_BackEnd/src/test/java/com/zensar/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.zensar.service;
+
+import com.zensar.dto.Admin;
+
+public class AdminService {
+
+    public Admin registerAdmin(Admin admin) {
+	// TODO Auto-generated method stub
+	return admin;
+    }
+
+}

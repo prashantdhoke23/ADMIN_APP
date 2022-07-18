428fa423f7ae101ebc897a5800c9244d5e9a6234
raja <raja.m@zensar.com>
2022-07-18 20:27:17 +0530

    added h2 db config

--- Admin_RestAPI/src/main/java/com/zensar/entity/AdminEntity.java
+++ Admin_RestAPI/src/main/java/com/zensar/entity/AdminEntity.java
@@ -1,6 +1,17 @@
 package com.zensar.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Admin")
 public class AdminEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
     private String name;
     private String password;
     private String email;
@@ -8,6 +19,18 @@ public class AdminEntity {
 
     public String getName() {
 	return name;
+    }
+
+    public AdminEntity() {
+	super();
+    }
+
+    public Integer getId() {
+	return id;
+    }
+
+    public void setId(Integer id) {
+	this.id = id;
     }
 
     public void setName(String name) {

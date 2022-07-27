9be7ded1873babcfdbf20f653b034aef5037c63c
SR66171 <SR66171@AESW42SLPC6437.ind.zensar.com>
2022-07-27 22:10:32 +0530

    Swagger implementation

--- Admin_RestAPI/src/main/java/com/zensar/AdminRestApiApplication.java
+++ Admin_RestAPI/src/main/java/com/zensar/AdminRestApiApplication.java
@@ -1,11 +1,23 @@
 package com.zensar;
 
+import java.util.ArrayList;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AdminRestApiApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +26,25 @@ public class AdminRestApiApplication {
 	@Bean 
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public Docket getCustomizedDocket() {
+	return new Docket(DocumentationType.SWAGGER_2)
+	.select()
+	.apis(RequestHandlerSelectors.basePackage("com.zensar"))
+	.paths(PathSelectors.any())
+	.build()
+	.apiInfo(getApiInfo());
+
+	}
+	private ApiInfo getApiInfo() {
+	ApiInfo apiInfo=new ApiInfo("Stock Rest Api Documentation",
+	"This page gives Admin Rest API Doucmentation ", "2.5",
+	"My Terms of services",
+	new Contact("Admin Services System","http://localhot:7779","admin-team@zensar.com"),
+	"GPL", "http://gpl.org",
+	new ArrayList<VendorExtension>());
+	return apiInfo;
 	}
 }
